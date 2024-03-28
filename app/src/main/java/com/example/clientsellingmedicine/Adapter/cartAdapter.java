6bdbfebeb491a1f1644b1f9c502957d7b8aa50c9
Pham Dang Khoa <156926024+phamdangkhoagh@users.noreply.github.com>
2024-03-28 14:30:10 +0700

    refactor: cart_screen and home_screen

--- app/src/main/java/com/example/clientsellingmedicine/Adapter/cartAdapter.java
+++ app/src/main/java/com/example/clientsellingmedicine/Adapter/cartAdapter.java
@@ -1,11 +1,13 @@
 package com.example.clientsellingmedicine.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,53 +19,42 @@ import com.bumptech.glide.Glide;
 import com.example.clientsellingmedicine.R;
 import com.example.clientsellingmedicine.models.Cart;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class cartAdapter extends RecyclerView.Adapter<cartAdapter.ViewHolder> {
 
-    private List<Cart> listProductSelected;
+    public static List<Cart> listProductsCart;
+
+    public static List<Cart> listProductsToBuy = new ArrayList<>();
+
     private Context mContext;
 
-    public CheckBox checkboxCartItem,masterCheckboxCart;
 
     public cartAdapter(List<Cart> list){
-        this.listProductSelected = list;
+        this.listProductsCart = list;
+    }
 
+    public List<Cart> getListProductsCart() {
+        return listProductsCart;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView tvNameCartItem, tvPriceCartItem;
+        public TextView tvNameCartItem, tvPriceCartItem,tvTotalAmountCart,tvQuantityCartItem;
         public ImageView ivCartItem;
 
-        private SparseBooleanArray itemStateArray= new SparseBooleanArray();
-
-
+        public CheckBox checkboxCartItem,masterCheckboxCart;
 
         public ViewHolder (View itemView, Context context) {
             super(itemView);
             tvNameCartItem = itemView.findViewById(R.id.tvNameCartItem);
             tvPriceCartItem = itemView.findViewById(R.id.tvPriceCartItem);
             ivCartItem = itemView.findViewById(R.id.ivCartItem);
-
             masterCheckboxCart = itemView.findViewById(R.id.masterCheckboxCart);
-            CheckBox checkboxCartItem = itemView.findViewById(R.id.checkboxCartItem);
-
-            itemView.setOnClickListener(this);
-            this.setIsRecyclable(false);
-
-            masterCheckboxCart.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int adapterPosition = getAdapterPosition();
-                    if (!itemStateArray.get(adapterPosition, false)) {
-                        checkboxCartItem.setChecked(true);
-                        itemStateArray.put(adapterPosition, true);
-                    } else {
-                        checkboxCartItem.setChecked(false);
-                        itemStateArray.put(adapterPosition, false);
-                    }
-                }
-            });
+            checkboxCartItem = itemView.findViewById(R.id.checkboxCartItem);
+            tvTotalAmountCart = itemView.findViewById(R.id.tvTotalAmountCart);
+            tvQuantityCartItem = itemView.findViewById(R.id.tvQuantityCartItem);
         }
     }
 
@@ -81,23 +72,47 @@ public class cartAdapter extends RecyclerView.Adapter<cartAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull cartAdapter.ViewHolder holder, int position) {
-        Cart cart = (Cart) listProductSelected.get(position);
-
+        Cart cart = listProductsCart.get(position);
         holder.tvNameCartItem.setText(cart.getName());
+        int quantity = cart.getQuantity();
+        holder.tvQuantityCartItem.setText(String.valueOf(quantity));
         String price = convertPrice(cart.getPrice());
         holder.tvPriceCartItem.setText(price+" đ");
+
+        holder.checkboxCartItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int position = holder.getAdapterPosition();
+                if (isChecked){
+                    Cart cart = listProductsCart.get(position);
+                    listProductsToBuy.add(cart);
+
+                    if (listProductsToBuy.contains(cart)) {
+                        Log.d("TAG", "listProductsToBuy contains cart ID: " + cart.getId() + " Price" + cart.getPrice());
+                    } else {
+                        Log.d("TAG", "listProductsToBuy does not contain cart");
+                    }
+                }else{
+                    for (int i = 0;i<listProductsToBuy.size();i++){
+                        if (listProductsToBuy.get(i).getId() == cart.getId()){
+                            listProductsToBuy.remove(i);
+                        }
+                    }
+                }
+            }
+        });
+
+
         Glide.with(holder.itemView.getContext())
                 .load(cart.getImage())
                 .placeholder(R.drawable.loading_icon) // Hình ảnh thay thế khi đang tải
                 .error(R.drawable.error_image) // Hình ảnh thay thế khi có lỗi
                 .into(holder.ivCartItem);
-
-        holder.setIsRecyclable(false);
     }
 
     @Override
     public int getItemCount() {
-        return listProductSelected.size();
+        return listProductsCart.size();
     }
     public String convertPrice(double number) {
         long integerPart = (long) number;
--- app/src/main/java/com/example/clientsellingmedicine/CartActivity.java
+++ app/src/main/java/com/example/clientsellingmedicine/CartActivity.java
@@ -1,30 +1,31 @@
 package com.example.clientsellingmedicine;
 
+
+import static com.example.clientsellingmedicine.Adapter.cartAdapter.listProductsCart;
+
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.clientsellingmedicine.Adapter.cartAdapter;
-import com.example.clientsellingmedicine.Adapter.productAdapter;
 import com.example.clientsellingmedicine.models.Cart;
-import com.example.clientsellingmedicine.models.Product;
 import com.example.clientsellingmedicine.services.CartService;
-import com.example.clientsellingmedicine.services.IdeaService;
 import com.example.clientsellingmedicine.services.ServiceBuilder;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,13 +36,22 @@ import retrofit2.Response;
 
 public class CartActivity extends AppCompatActivity {
     private Context mContext;
-    RecyclerView rcvCart;
+
+//    List<Cart> listProductsCart = new ArrayList<>(); // Your list of carts
+
     cartAdapter cartAdapter;
+//    cartAdapter adapterCart = new cartAdapter(listProductsCart);
+    RecyclerView rcvCart;
     LinearLayout bottom_view,linear_layout_dynamic;
+
+    TextView tvTotalAmountCart;
+
 
     ImageView icon_arrow_up;
 
     CheckBox checkboxCartItem,masterCheckboxCart;
+
+//    private List<Cart> listProductsToBuy;
 
 
 
@@ -62,53 +72,23 @@ public class CartActivity extends AppCompatActivity {
         bottom_view = findViewById(R.id.bottom_view);
         linear_layout_dynamic = findViewById(R.id.linear_layout_dynamic);
         icon_arrow_up = findViewById(R.id.icon_arrow_up);
-        checkboxCartItem = findViewById(R.id.checkboxCartItem);
-        masterCheckboxCart = findViewById(R.id.masterCheckboxCart);
+        tvTotalAmountCart = findViewById(R.id.tvTotalAmountCart);
+
+//        checkboxCartItem = findViewById(R.id.checkboxCartItem);
+//        masterCheckboxCart = findViewById(R.id.masterCheckboxCart);
     }
 
     private void addEvents() {
+        Log.d("j", "--->this function addEvents: " );
         getCartItems();
+        Log.d("j", "--->After getCartItem: " );
+//        Log.d("TAG", "totalAmount: " + totalAmount);
+//        Log.d("j", "total: " + totalCartItem);
 
-//        masterCheckboxCart.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                checkboxCartItem.setChecked(isChecked);
-//            }
-//        });
+        double totalAmount = calculateTotalAmount();
+        String totalCartItem = convertPrice(totalAmount);
+        tvTotalAmountCart.setText(totalCartItem);
 
-        // Listener for the individual checkboxes
-//        checkboxCartItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (!isChecked) {
-//                    masterCheckboxCart.setChecked(false);
-//                } else {
-//                    // Check if all checkboxes are checked
-//                    boolean allChecked = areAllCheckboxesChecked();
-//                    // Update masterCheckbox's state
-//                    if(allChecked) {
-//                        masterCheckboxCart.setChecked(true);
-//                    }
-//                }
-//            }
-//        });
-
-
-//        checkboxCartItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//                    // Đánh dấu CheckBox đã được tích
-//                    checkboxCartItem.setChecked(true);
-//                    isChecked = true;
-//                } else {
-//                    // Đánh dấu CheckBox chưa được tích
-//                    checkboxCartItem.setChecked(false);
-//                    isChecked = false;
-//                }
-//            }
-//        });
-        //  display detail price cart
         icon_arrow_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -127,7 +107,7 @@ public class CartActivity extends AppCompatActivity {
                     bottom_view.setLayoutParams(layoutParams);
 
                     //display view
-                    linear_layout_dynamic.setVisibility(v.VISIBLE);
+                    linear_layout_dynamic.setVisibility(View.VISIBLE);
                     // set icon down
                    icon_arrow_up.setImageResource(R.drawable.ic_arrow_down);
                 }
@@ -143,19 +123,15 @@ public class CartActivity extends AppCompatActivity {
                     bottom_view.setLayoutParams(layoutParams);
 
                     //display view
-                    linear_layout_dynamic.setVisibility(v.GONE);
+                    linear_layout_dynamic.setVisibility(View.GONE);
                     // set icon up
                     icon_arrow_up.setImageResource(R.drawable.ic_arrow_up);
                 }
 
             }
         });
-
-
-        //set state checkbox
-
-
     }
+
 
     public void getCartItems(){
         CartService cartService = ServiceBuilder.buildService(CartService.class);
@@ -188,19 +164,28 @@ public class CartActivity extends AppCompatActivity {
         });
     }
 
-    // Function to check if all checkboxes are checked
-    private boolean areAllCheckboxesChecked() {
-        // Add other checkboxes to this list as needed
-        List<CheckBox> checkboxes = new ArrayList<>();
-        checkboxes.add(checkboxCartItem);
-        // Add other checkboxes here
+    public String convertPrice(double number) {
+        long integerPart = (long) number;
+        int decimalPart = (int) ((number - integerPart) * 1000);
 
-        for (CheckBox checkbox : checkboxes) {
-            if (!checkbox.isChecked()) {
+        String formattedIntegerPart = String.format("%,d", integerPart).replace(",", ".");
+        String formattedDecimalPart = String.format("%03d", decimalPart);
+
+        return formattedIntegerPart + "." + formattedDecimalPart;
+    }
+
+    private double calculateTotalAmount() {
+        double totalAmount = 0;
+        if (cartAdapter.listProductsToBuy != null) {
+            Log.d("TAG", "--------cartAdapter-----:  " + cartAdapter.listProductsToBuy.size());
+            for (int i = 0; i < cartAdapter.listProductsToBuy.size(); i++) {
+                Cart cart = cartAdapter.listProductsToBuy.get(i);
+
+                totalAmount += cart.getPrice() * cart.getQuantity();
             }
-            return false;
         }
-        return true;
+        Log.d("TAG", "calculateTotalAmount: " + totalAmount);
+        return totalAmount;
     }
 
 }
