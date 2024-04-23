c059c58aaa3bbe892a876a578e7ee4ce4073d2ed
vancong2305 <truongvancong230502@gmail.com>
2024-04-24 03:58:21 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/clientsellingmedicine/LoginActivity.java
+++ app/src/main/java/com/example/clientsellingmedicine/LoginActivity.java
@@ -48,7 +48,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LoginActivity  extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private Context mContext;
 
     TextInputEditText edt_phone_number, edt_password;
